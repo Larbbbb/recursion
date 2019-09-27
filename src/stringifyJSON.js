353b552702fc408b623c138066310eb453cd834f
Laurence <ldn221@nyu.edu>
2019-09-27 12:45:30 -0700

    Make spec pass for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,46 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+  	return "null";
+
+  } else if (Array.isArray(obj)) {
+  	var stringValue = `[`;
+
+
+  	for (var i = 0; i < obj.length; i++) {
+  		if (typeof obj[i] === "undefined" || typeof obj[i] === "function") {
+  			continue;
+  		}
+  		stringValue += `${stringifyJSON(obj[i])}`;
+  		if (i < obj.length - 1) {
+  			stringValue += `,`;
+  		}
+  	}
+  	stringValue += `]`;
+  	return stringValue;
+
+  } else if (typeof obj === "object") {
+	var stringValue = `{`;
+
+  	var objectKeys = Object.keys(obj);
+  	for (var i = 0; i < objectKeys.length; i++) {
+  		if (typeof obj[objectKeys[i]] === "undefined" || typeof obj[objectKeys[i]] === "function") {
+  			continue;
+  		}
+  		stringValue += `"${objectKeys[i]}":${stringifyJSON(obj[objectKeys[i]])}`;
+  		if (i < objectKeys.length - 1) {
+  			stringValue += `,`;
+  		}
+  	}
+  	stringValue += `}`;
+	return stringValue;
+
+  } else if (typeof obj === "string") {
+  	return `"${obj}"`;
+
+  } else {
+	return obj.toString();
+  
+  }
 };
